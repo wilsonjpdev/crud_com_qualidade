ce5f026526520ec49a5bf318ab929f460e086a45
wilsonjp <wilsonjp.dev@outlook.com>
2024-03-04 19:15:05 -0300

    adiconado update e delete

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 import fs from 'fs'
 const dbFilePath = "./db"
+import { v4 as uuid } from "uuid"
 
 interface Todo {
+    id: string,
     date: string,
     content: string,
     done:boolean
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false
@@ -20,7 +23,7 @@ function create(content: string) {
     ]
     
     fs.writeFileSync(dbFilePath, JSON.stringify({todos}, null, 2))
-    return content
+    return todo
 }
 
 function read(): Array<Todo> {
@@ -32,5 +35,47 @@ function read(): Array<Todo> {
     return db.todos
 }
 
-create("Nova Todo")
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo
+    const todos = read()
+    todos.forEach(currentTodo => {
+        const isToUpdate = currentTodo.id === id
+        if(isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo)
+        }
+    })
+    fs.writeFileSync(dbFilePath, JSON.stringify({
+        todos
+    }, null, 2))
+    if(!updatedTodo) {
+        throw new Error("Please, provide a new ID")
+    }
+    return updatedTodo
+}
+
+function deleteById(id: string) {
+    const todos = read()
+    const todoWithoutOne = todos.filter(todo => {
+        if(id === todo.id) {
+            return false
+        }
+        return true
+    })
+    fs.writeFileSync(dbFilePath, JSON.stringify({
+        todos: todoWithoutOne
+    }, null, 2))
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(dbFilePath, "")
+}
+
+CLEAR_DB()
+create("Primeira todo")
+const secondTodo = create("Segunda todo")
+deleteById(secondTodo.id)
+const ThirdTodo = create("Terceira todo")
+update(ThirdTodo.id, {
+    content: "Atualizado"
+})
 console.log(read())
